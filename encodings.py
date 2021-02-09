6562c6d5aba82fda0f3a488b6918e190df4ae0aa
Jten Saini <saini.jiten10@gmail.com>
2021-02-08 19:41:05 -0500

    second commit

--- encodings.py
+++ encodings.py
@@ -3,17 +3,17 @@ import face_recognition
 
 enc = []
 
-picture_of_me = face_recognition.load_image_file("cayden.png")
+picture_of_me = face_recognition.load_image_file("xxx.png")
 my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
 
 enc.append(my_face_encoding)
 
-unknown_picture = face_recognition.load_image_file("jiten.jpeg")
+unknown_picture = face_recognition.load_image_file("yyyy.jpeg")
 unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
 
 enc.append(unknown_face_encoding)
 
-unknown_picture1 = face_recognition.load_image_file("zhao.jpeg")
+unknown_picture1 = face_recognition.load_image_file("zzzz.jpeg")
 unknown_face_encoding1 = face_recognition.face_encodings(unknown_picture1)[0]
 
 enc.append(unknown_face_encoding1)
